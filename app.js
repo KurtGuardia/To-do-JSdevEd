9ec9b37ecfe4ae43d24acb8dfc17a062844452da
Kurt Guardia <kurtguardia@gmail.com>
2020-05-29 17:51:24 +0200

    Added delete functionality

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list')
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 function addTodo(event) {
@@ -31,4 +32,14 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
     //Celar Todo input value
     todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    //Detele To-do
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+        
 }
